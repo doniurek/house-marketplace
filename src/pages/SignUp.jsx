1f8a16252f990732fdd406e89af9c2e29e737e70
doniurek <aldonaglebicka@gmail.com>
2022-09-12 22:16:17 +0200

    Save User To Firestore

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
+import { setDoc, doc, serverTimestamp } from "firebase/firestore"
 import { db } from "../firebase.config"
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
@@ -36,6 +37,12 @@ function SignUp() {
       updateProfile(auth.currentUser, {
         displayName: name
       })
+
+      const formDataCopy = {...formData} /*copy bc we don't want to change formData state*/
+      delete formDataCopy.password /*do not submit password to the data base */
+      formDataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy) /* await bc setDoc returns promise */
 
       navigate("/")
 
