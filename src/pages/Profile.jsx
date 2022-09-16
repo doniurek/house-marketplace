0bf1e9f10f540a2eb8b96c402b4c92f1fd402ce5
doniurek <aldonaglebicka@gmail.com>
2022-09-16 17:57:18 +0200

    Edit Listing Icon

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -91,6 +91,10 @@ function Profile() {
     }
   }
 
+  const onEdit = (listingId) => {
+    navigate(`/edit-listing/:${listingId}`)
+  }
+
   return <div className="profile">
     <header className="profileHeader">
       <p className="pageHeader">My Profile</p>
@@ -141,7 +145,13 @@ function Profile() {
           <p className="listingText">Your Listings</p>
           <ul className="listingsList">
             {listings.map((listing) => (
-              <ListingItem key={listing.id} listing={listing.data} id={listing.id} onDelete={() => onDelete(listing.id)}/>
+              <ListingItem
+                key={listing.id}
+                listing={listing.data}
+                id={listing.id}
+                onDelete={() => onDelete(listing.id)}
+                onEdit={() => onEdit(listing.id)}
+              />
             ))}
           </ul>
         </>
